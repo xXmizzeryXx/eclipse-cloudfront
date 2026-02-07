b55749c2a13e5d4367a6fe13ad2345f13bb49899
mizz <mroytinkercad@gmail.com>
2026-02-06 18:14:02 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,47 +1,9 @@
 export default {
-  async fetch(request, env) {
-    const url = new URL(request.url);
-    
-    // CORS headers
-    const corsHeaders = {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, HEAD, POST, OPTIONS',
-      'Access-Control-Allow-Headers': '*',
-    };
-    
-    // Handle preflight OPTIONS request
-    if (request.method === 'OPTIONS') {
-      return new Response(null, {
-        status: 204,
-        headers: corsHeaders
-      });
-    }
-    
-    // Redirect / to /index.html
+  fetch(request, env) {
+    const url = new URL(request.url)
     if (url.pathname === "/") {
-      url.pathname = "/index.html";
+      url.pathname = "/index.html"
     }
-    
-    try {
-      // Fetch the asset
-      const response = await env.ASSETS.fetch(url);
-      
-      // Create new response with CORS headers
-      const newHeaders = new Headers(response.headers);
-      newHeaders.set('Access-Control-Allow-Origin', '*');
-      newHeaders.set('Access-Control-Allow-Methods', 'GET, HEAD, POST, OPTIONS');
-      newHeaders.set('Access-Control-Allow-Headers', '*');
-      
-      return new Response(response.body, {
-        status: response.status,
-        statusText: response.statusText,
-        headers: newHeaders
-      });
-    } catch (e) {
-      return new Response('Not Found', { 
-        status: 404,
-        headers: corsHeaders
-      });
-    }
+    return env.ASSETS.fetch(url)
   }
 }
